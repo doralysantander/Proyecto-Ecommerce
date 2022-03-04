057bc59402f6716eb745d87672c6fda7763ed34e
doraly9110@hotmail.com <doraly9110@hotmail.com>
2022-03-04 00:56:26 -0500

    Agregue a Navbar.js los items menu hamburgesa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+//import {Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Navbar />
+    <h1 className="text-3xl text-blue-600  bg-yellow-600	font-bold underline">
+      Hello world!
+    </h1>
+     <Navbar />
     </>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,20 +3,44 @@ import './navbar.css'
 import logo from '../../assets/logo/logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faCartPlus } from '@fortawesome/free-solid-svg-icons'
-//import { GiHamburgerMenu } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom'
+
 
 const Navbar = () => {
-    
+
     const [showMenu, setshowMenu]= useState(false);//valor estado
+    const [open, setOpen] = useState(false);
     let menu 
     if(showMenu){
-        menu=<div>the menu</div>
+        
+        <nav>
+            <ul className={open ? 'nav-links active':'nav-links'}>
+                <li className="nav-item">
+                    <navLink to='/' className="nav-link"onClick={()=>setOpen(false)}>
+                    Inicio
+                    </navLink>
+                </li>
+                <li className="nav-item">
+                    <navLink to='/' className="nav-link"onClick={()=>setOpen(false)}>
+                   Quienes somos
+                    </navLink>
+                </li>
+                <li className="nav-item">
+                    <navLink to='/'className="nav-link"onClick={()=>setOpen(false)}>
+                   Productos
+                    </navLink>
+                </li>
+
+            </ul>   
+
+
+        </nav>
+        //menu=<div>the menu</div>
     }
 
   return (
     <>
     <div className="navbarContainer">
-
     <FontAwesomeIcon icon={faBars} className="icons"
      onClick={()=>setshowMenu(!showMenu)}//true
     />
@@ -24,7 +48,6 @@ const Navbar = () => {
         src={logo}
         alt={""}
         className="img-logo"
-       
         />
 
     <FontAwesomeIcon icon={faCartPlus} className="icons"/>
