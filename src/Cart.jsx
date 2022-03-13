f51cefa736d07431a3f8218b8512bb99a9ecf07c
doraly9110@hotmail.com <doraly9110@hotmail.com>
2022-03-13 17:10:11 -0500

    implementacion carrito pero no muestra el precio total

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -7,6 +7,7 @@ const Cart = () => {
         totalUniqueItems,
         items,
         totalItems,
+        carTotal,
         updateItemQuantity,
         removeItem,
         emptyCart,
@@ -17,9 +18,41 @@ const Cart = () => {
         <div>
             <div>
                 <h5>Cart({totalUniqueItems}) Total:items({totalItems}) </h5>
+                <div>
+                    {items.map((item, index) => {
+                        return (
+                        <div className=" shadow-black w-full flex gap-4 items-center "key={index}> 
+                          <div className="flex flex-col">
+                            <img src={item.image} alt="" className="w-3/6 h-3/6 overflow-hidden aspect-square"/>
+                            <p>{item.description}</p>
+                         </div>
+                         <div className="flex gap-8">
+                            <span>Precio:{item.price}</span>
+                            <span>Cantidad({item.quantity})</span>
+                            </div>
+                            <button onClick={()=>updateItemQuantity(item.id, item.quantity - 1)}
+                            >-
+                            </button>
 
+                            <button onClick={()=>updateItemQuantity(item.id,  item.quantity + 1)}>
+                                +
+                            </button>
+
+                            <button onClick={()=> removeItem(item.id)}>
+                               Borrar 
+                            </button>
+                            
+                        </div>
+                        
+                        )
+
+                    })}
+                </div>
+              
             </div>
-            
+            <div> 
+                 <h2>Precio Total:{carTotal}</h2>
+                </div>
         </div>
       </section>
     
